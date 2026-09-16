10ff44656bfc26fdeebb29fc40eea60e7096b7e7
Mael Favennec <maelfavennec29760@gmail.com>
2026-09-16 10:05:28 +0200

    feat: add endpoint to get a book by ID / add Cors Support

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,5 +4,6 @@ const bookCtrl = require('../controllers/books');
 const router = express.Router();
 
 router.get('/', bookCtrl.getAllBook)
+router.get('/:id', bookCtrl.getBookById)
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose')
-const userRoutes = require('./routes/user.js')
-const booksRoutes = require('./routes/book.js')
-const app = express()
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoutes = require('./routes/user.js');
+const booksRoutes = require('./routes/book.js');
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth', userRoutes)
-app.use('/api/books', booksRoutes)
+app.use('/api/auth', userRoutes);
+app.use('/api/books', booksRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("Connection to MongoDB successful"))
     .catch((error) => console.log("connection to MongoDB failed", error));
 
 app.listen(4000, () => {
-    console.log("Server running on port 4000 :)")
+    console.log("Server running on port 4000 :)");
 })
